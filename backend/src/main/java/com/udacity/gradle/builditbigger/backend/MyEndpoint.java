5738c983de90df0968d9f0738e2a04047c546425
Javier Vieira <xavi.vieira@gmail.com>
2018-05-28 08:16:55 +0200

    delete code unused

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,7 +3,6 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-import com.google.api.server.spi.config.Named;
 
 import unxavi.com.github.jokejavalib.Joke;
 
@@ -20,17 +19,6 @@ import unxavi.com.github.jokejavalib.Joke;
         )
 )
 public class MyEndpoint {
-
-    /**
-     * A simple endpoint method that takes a name and says Hi back
-     */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
-        MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
-        return response;
-    }
 
     /**
      * A simple endpoint method that return a random joke
